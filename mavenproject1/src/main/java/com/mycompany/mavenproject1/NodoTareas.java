9a51397afb810185be19ec420661842a7c1768a7
Jhoana Aquino <jhoana.aquino10@gmail.com>
2024-10-16 15:55:36 -0500

    nueva actualizacion :v

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NodoTareas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author jhoan
+ */
+public class NodoTareas {
+    String tarea;
+    int prioridad;
+    NodoTareas siguiente;
+
+    public NodoTareas(String tarea, int prioridad) {
+        this.tarea = tarea;
+        this.prioridad = prioridad;
+        this.siguiente = null;
+    }
+}
